eeb97dd05e43a7292064ac537385ea786f07c485
Saurabh Dabas <developerdabas@gmail.com>
2022-02-09 18:48:20 +0000

    Finding the middle elements of the array

---
+++ middle.js
@@ -0,0 +1,51 @@
+function eqArrays(arr1,arr2){
+  if(arr1.length != arr2.length){
+    return false
+  }
+  else{
+    for(let i = 0 , j = 0 ; i < arr1.length , j < arr2.length ; i ++ , j ++ ){
+      if(arr1[i] !== arr2[j]){
+        return false;
+      }
+    }
+    return true
+  }
+}
+// function assertArraysEqual(arr1,arr2){
+//   if (eqArrays(arr1,arr2) === true) {
+//     console.log(`✅✅✅ Assertion Passed`);
+//   } else {
+//     console.log(`🔴🔴🔴 Assertion Failed`);
+//   }
+// }
+
+
+const middle = arr => {
+  if (arr.length <= 2) {
+    return [];
+  }
+  if (arr.length % 2 == 1) {
+    return [arr[Math.floor(arr.length / 2)]];
+  }
+  if (arr.length % 2 == 0) {
+    return [arr[arr.length / 2 - 1], arr[arr.length / 2]];
+  }
+};
+console.log(middle([1,2]));  
+console.log(middle([1,2,3]));
+console.log(middle([1,2,3,4]));
+
+function assertMiddle(midVal,arr){
+  console.log("a",midVal,"b",arr)
+if ( eqArrays(midVal,arr) ){
+  console.log(`✅✅✅ Assertion Passed`);
+}
+else {
+  console.log(`🔴🔴🔴 Assertion Failed`);
+}
+}
+assertMiddle(middle([1,2]),[])
+assertMiddle(middle([1,2,3]),[2])
+assertMiddle(middle([1,2,3,4]),[2,3])
+
+
