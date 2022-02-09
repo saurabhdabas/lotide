e0d854a8acad26b72ee6699ba4ea3bf884916217
Saurabh Dabas <developerdabas@gmail.com>
2022-02-09 19:58:03 +0000

    Counting the letters

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters= function (allItems){
+  let results ={};
+  for (const item of allItems) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+  }
+  return results;
+}
+console.log(countLetters("LHL"));
+
+const result1 = countLetters("LHL");
+assertEqual(result1["L"], 2);
