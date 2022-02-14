c57c18a522cfc7565b9ab760679c2dec4f193aab
saurabhdabas <developerdabas@gmail.com>
2022-02-14 21:09:48 +0000

    Refactoring my code using mocha and chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,9 +1,25 @@
-const assertEqual = require('../assertEqual');
+const assert = require("chai").assert;
 const eqArrays = require('../eqArrays');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // ----> should Pass
-assertEqual(eqArrays([1, 2, 3], [1, 4, 3]), true);// ----> should fail
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // ----> should Pass
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // ----> should fail
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3" , "4"]), true); // ----> should fail
-assertEqual(eqArrays(["1", "2", "3"], []), true); // ----> should fail
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // ----> should Pass
+// assertEqual(eqArrays([1, 2, 3], [1, 4, 3]), true);// ----> should fail
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // ----> should Pass
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // ----> should fail
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3" , "4"]), true); // ----> should fail
+// assertEqual(eqArrays(["1", "2", "3"], []), true); // ----> should fail
+
+
+
+
+
+describe("eqArrays",()=>{
+  it("it should pass",()=>{
+    assert.isTrue(eqArrays([1, 2, 3], [1, 2, 3],true))
+  }),
+  it("it should pass",()=>{
+    assert.isFalse(eqArrays([1, 2, 3], [1, 4, 3],false))
+  }),
+  it("it should pass",()=>{
+  assert.isFalse(eqArrays(["1", "2", "3"], ["1", "2", "3" , "4"]), true)
+  })
+})
