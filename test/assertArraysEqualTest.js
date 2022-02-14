cca03742226ae7b2d8d08ba6aab59261c7e1f26f
saurabhdabas <developerdabas@gmail.com>
2022-02-14 21:00:55 +0000

    Refactoring using mocha and chai

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,5 +1,18 @@
-const assertArraysEqual = require("../assertArraysEqual")
+const assert = require("chai").assert;
+// const assertArraysEqual = require("../assertArraysEqual")
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // should pass
-assertArraysEqual([1, 2, 3], [1, 2]); // should fail
-assertArraysEqual([1, 2, 3], [1, 2, "3"]); // should fail
+// assertArraysEqual([1, 2, 3], [1, 2, 3]); // should pass
+// assertArraysEqual([1, 2, 3], [1, 2]); // should fail
+// assertArraysEqual([1, 2, 3], [1, 2, "3"]); // should fail
+
+describe("assertArraysEqual",()=>{
+  it("it should pass",()=>{
+    assert.deepEqual([1, 2, 3], [1, 2, 3])
+  }),
+  it("it should Fail",()=>{
+    assert.notDeepEqual([1, 2, 3], [1, 2])
+  }),
+  it("it should Fail",()=>{
+    assert.notDeepEqual([1, 2, 3], [1, 2, "3"])
+  })
+})
