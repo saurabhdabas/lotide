c67802f7e12420c10b6705054240457c7efc6c03
saurabhdabas <developerdabas@gmail.com>
2022-02-14 21:04:52 +0000

    Refactoring using mocha and chai

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,3 +1,12 @@
+const assert = require("chai").assert;
 const assertEqual = require('../assertEqual');
 
-assertEqual("LighthouseLabs","Bootcamp");
+// assertEqual("LighthouseLabs","Bootcamp");
+describe("assertEqual",()=>{
+  it("it should fail",()=>{
+    assert.notDeepEqual("LighthouseLabs","Bootcamp")
+  }),
+  it("it should pass",()=>{
+    assert.deepEqual("hello","hello")
+  })
+})
