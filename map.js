60412a703d56090f4d396ff3f7df028a38ddc37e
saurabhdabas <developerdabas@gmail.com>
2022-02-14 20:16:32 +0000

    Refactoring using module.exports

--- map.js
+++ map.js
@@ -1,22 +1,22 @@
-function eqArrays(arr1,arr2) {
-  if (arr1.length != arr2.length) {
-    return false;
-  } else {
-    for (let i = 0 , j = 0; i < arr1.length , j < arr2.length; i ++ , j ++) {
-      if (arr1[i] !== arr2[j]) {
-        return false;
-      }
-    }
-    return true;
-  }
-}
-function assertArraysEqual(arr1,arr2) {
-  if (eqArrays(arr1,arr2) === true) {
-    console.log(`✅✅✅ Assertion Passed`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed`);
-  }
-}
+// function eqArrays(arr1,arr2) {
+//   if (arr1.length != arr2.length) {
+//     return false;
+//   } else {
+//     for (let i = 0 , j = 0; i < arr1.length , j < arr2.length; i ++ , j ++) {
+//       if (arr1[i] !== arr2[j]) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+// }
+// function assertArraysEqual(arr1,arr2) {
+//   if (eqArrays(arr1,arr2) === true) {
+//     console.log(`✅✅✅ Assertion Passed`);
+//   } else {
+//     console.log(`🔴🔴🔴 Assertion Failed`);
+//   }
+// }
 
 
 const words = ["ground", "control", "to", "major", "tom"];
@@ -31,10 +31,11 @@ const map = function(array, callback) {
 const results1 = map(words, word => word[0]);
 console.log(results1);
 
-assertArraysEqual(results1, ['g','c','t','m','t']); // should pass
+module.exports = {map,results1,words};
+// assertArraysEqual(results1, ['g','c','t','m','t']); // should pass
 
-assertArraysEqual(map(words, word => word[0]), ['g','c','t','m','t']); // should pass
+// assertArraysEqual(map(words, word => word[0]), ['g','c','t','m','t']); // should pass
 
-assertArraysEqual(map(words, word => word[0]), ['g','c','t','m','t']); // should pass
+// assertArraysEqual(map(words, word => word[0]), ['g','c','t','m','t']); // should pass
 // assertArraysEqual([1, 2, 3], [1, 2]); // should fail
 // assertArraysEqual([1, 2, 3], [1, 2, "3"]); // should fail
