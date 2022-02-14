3ac987eaf9a99b8cfd87795f9c20e1c314f0de20
saurabhdabas <developerdabas@gmail.com>
2022-02-14 20:25:25 +0000

    refactoring using module.exports

--- flatten.js
+++ flatten.js
@@ -1,22 +1,22 @@
-const eqArrays = function(arr1,arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let i = 0 , j = 0; i < arr1.length , j < arr2.length; i ++ , j ++) {
-      if (arr1[i] !== arr2[j]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
-const assertArraysEqual = function(arr1,arr2) {
-  if (eqArrays(arr1,arr2) === true) {
-    console.log(`✅✅✅ Assertion Passed`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed`);
-  }
-};
+// const eqArrays = function(arr1,arr2) {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   } else {
+//     for (let i = 0 , j = 0; i < arr1.length , j < arr2.length; i ++ , j ++) {
+//       if (arr1[i] !== arr2[j]) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+// };
+// const assertArraysEqual = function(arr1,arr2) {
+//   if (eqArrays(arr1,arr2) === true) {
+//     console.log(`✅✅✅ Assertion Passed`);
+//   } else {
+//     console.log(`🔴🔴🔴 Assertion Failed`);
+//   }
+// };
 
 
 const flatten = function(array) {
@@ -32,9 +32,10 @@ const flatten = function(array) {
   }
   return output;
 };
-flatten([1, 2, [3, 4], 5, [6],[7,8,9]]); // => [1, 2, 3, 4, 5, 6]
+module.exports = flatten;
+// flatten([1, 2, [3, 4], 5, [6],[7,8,9]]); // => [1, 2, 3, 4, 5, 6]
 
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6],[7,8,9]]), [1, 2, 3, 4, 5, 6]); // should fail
-assertArraysEqual(flatten([1, 2, 3]), [1, 2,3]); // should pass
-assertArraysEqual(flatten([1, 2, [3,4]]), [1, 2, 3,4]); // should pass
-assertArraysEqual(flatten([1, 2, [3,4]]), [1, 2, [3,4]]); // should fail
+// assertArraysEqual(flatten([1, 2, [3, 4], 5, [6],[7,8,9]]), [1, 2, 3, 4, 5, 6]); // should fail
+// assertArraysEqual(flatten([1, 2, 3]), [1, 2,3]); // should pass
+// assertArraysEqual(flatten([1, 2, [3,4]]), [1, 2, 3,4]); // should pass
+// assertArraysEqual(flatten([1, 2, [3,4]]), [1, 2, [3,4]]); // should fail
