adee4c962a373c03373875ee0cf819bc14327343
saurabhdabas <developerdabas@gmail.com>
2022-02-14 19:16:52 +0000

    Refactoring using mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,18 +1,35 @@
+// const eqArrays = require("../eqArrays")
+// const middle = require("../middle");
+
+
+// function assertMiddle(midVal,arr) {
+//   console.log("a",midVal,"b",arr);
+//   if (eqArrays(midVal,arr)) {
+//     console.log(`✅✅✅ Assertion Passed`);
+//   } else {
+//     console.log(`🔴🔴🔴 Assertion Failed`);
+//   }
+// }
+
+
+
+// assertMiddle(middle([1,2]),[]); //------should Pass
+// assertMiddle(middle([1,2,3]),[2]);//------should Pass
+// assertMiddle(middle([1,2,3,4]),[2,3]);//------should Pass
+
+
+const assert = require("chai").assert;
+const middle = require ("../middle");
 const eqArrays = require("../eqArrays")
-const middle = require("../middle");
 
-
-function assertMiddle(midVal,arr) {
-  console.log("a",midVal,"b",arr);
-  if (eqArrays(midVal,arr)) {
-    console.log(`✅✅✅ Assertion Passed`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed`);
-  }
-}
-
-
-
-assertMiddle(middle([1,2]),[]);
-assertMiddle(middle([1,2,3]),[2]);
-assertMiddle(middle([1,2,3,4]),[2,3]);
+describe("middle",()=>{
+  it("it should pass",()=>{
+    assert.deepEqual(middle([1,2]),[])
+  }),
+  it("it should pass",()=>{
+    assert.deepEqual(middle([1,2,3]),[2])
+  }),
+  it("it should pass",()=>{
+    assert.deepEqual(middle([1,2,3,4]),[2,3])
+  })
+})
